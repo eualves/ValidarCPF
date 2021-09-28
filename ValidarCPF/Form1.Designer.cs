fe1b6148e6c80eef847bd3a19e9513afd527ae60
Leonardo <leoalvesbico@gmail.com>
2021-09-28 15:26:50 -0300

    Adicionar arquivos de projeto.

---
+++ ValidarCPF/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace ValidarCPF
+{
+    partial class validadorCPF
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Validador de CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF";
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidar.Location = new System.Drawing.Point(215, 96);
+            this.btnValidar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(91, 23);
+            this.btnValidar.TabIndex = 3;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            this.btnValidar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnValidar_KeyPress);
+            // 
+            // lblResul
+            // 
+            this.lblResul.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblResul.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResul.Location = new System.Drawing.Point(12, 144);
+            this.lblResul.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(294, 146);
+            this.lblResul.TabIndex = 4;
+            this.lblResul.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(55, 96);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.maskedTextBox1.Mask = "000,000,000-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(123, 23);
+            this.maskedTextBox1.TabIndex = 5;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // validadorCPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 312);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "validadorCPF";
+            this.Text = "Validador de CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblResul;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ ValidarCPF/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidarCPF
+{
+    public partial class validadorCPF : Form
+    {
+        public validadorCPF()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+
+            string cpfdigitado = maskedTextBox1.Text.Replace(".","").Replace("-","").Replace(" ","");
+
+
+            if (string.IsNullOrWhiteSpace(cpfdigitado))
+            {
+                lblResul.Text = "Digite o CPF";
+                maskedTextBox1.Focus();
+                maskedTextBox1.SelectAll();
+                return;
+            }
+
+            if (cpfdigitado.Length != 11)
+            {
+                lblResul.Text = "Informe um CPF com 11 caracteres";
+                lblResul.ForeColor= Color.Red;
+                return;
+            }
+
+            //Separar os números em grupos
+
+            string cpf = cpfdigitado.Substring(0, 9);
+
+            int soma = 0;
+            int valorRef = 10;
+
+            for(int i = 0; i <= 8; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) * valorRef--;
+            }
+
+            int div1 = (int)soma % 11;
+
+            if (div1 < 2)
+            {
+                div1 = 0;
+            }
+            else
+            {
+                div1 = 11 - div1;
+            }
+
+            if (!cpfdigitado.Substring(9, 1).Equals(div1.ToString()))
+            {
+                lblResul.Text = "Informe um CPF válido";
+                lblResul.ForeColor = Color.Red;
+                return;
+            }
+
+            //Calcular o segundo digito verificador
+
+            soma = 0;
+            valorRef = 11;
+
+            cpf = cpf + div1;
+
+            for (int i=0;i<=9; i++)
+            {
+                soma += Convert.ToInt32(cpf[i].ToString()) *valorRef--;
+            }
+
+            int div2 = (int)(soma % 11);
+
+            if(div2 < 2)
+            {
+                div2 = 0;
+            }
+            else
+            {
+                div2 = 11 - div2;
+            }
+
+            if (!cpfdigitado.Substring(10, 1).Equals(div2.ToString()))
+            {
+                lblResul.Text = "CPF válido";
+                lblResul.ForeColor = Color.Green;
+                return;
+            }
+            lblResul.Text = "CPF válido";
+            lblResul.ForeColor = Color.Green;
+            return;
+        }
+
+        private void btnValidar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            btnValidar.Enabled.ToString();
+        }
+    }
+}
